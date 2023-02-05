e2e651c145472d6f3a676f7e193396d44b15f91f
Amanda Carvalho <amanda.chrys370@hotmail.com>
2023-02-05 18:03:41 -0400

    Criando Elemento e mostrando uma lista na tela

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,24 @@
 const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+const novoItem = document.createElement('li')
+
+//Capturando o elemento do formulário
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-
-    console.log(evento.target.elements['tarefa'].value)
+    criaElemento(evento.target.elements['tarefa'].value)
 })
 
+//Adicionando o elemento a minha lista
+
+function criaElemento(tarefa){
+
+    novoItem.classList.add("item")
+    novoItem.innerHTML = tarefa 
+    lista.appendChild(novoItem)
+    
+}
+
+//Adicionando ao LocalStorage
 
