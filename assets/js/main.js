291f44dcb41a2ae592af618cb0e76d532a353ed5
Amanda Carvalho <amanda.chrys370@hotmail.com>
2023-02-05 19:12:43 -0400

    Itens sendo armazenado no localStorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,37 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
-const novoItem = document.createElement('li')
+const itens = localStorage.getItem("itens") || []
+
 
 //Capturando o elemento do formulário
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-    criaElemento(evento.target.elements['tarefa'].value)
+    const tarefa = evento.target.elements['tarefa']
+
+    criaElemento(tarefa.value)
+
+    tarefa.value = ""
 })
 
 //Adicionando o elemento a minha lista
 
 function criaElemento(tarefa){
+    const novoItem = document.createElement('li')
 
     novoItem.classList.add("item")
     novoItem.innerHTML = tarefa 
     lista.appendChild(novoItem)
-    
+
+    const itemAtual = {
+        "tarefa": tarefa
+    }
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
 }
 
-//Adicionando ao LocalStorage
+
 
