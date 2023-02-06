3c174a9fcc0864f57e7b3ee1e1c31ec3ccaa24ef
Amanda Carvalho <amanda.chrys370@hotmail.com>
2023-02-05 22:26:31 -0400

    Inserido a exclusão dos itens e msg de item ja existente

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
-const itens = localStorage.getItem("itens") || []
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+itens.forEach((elemento) => {
+    criaElemento(elemento)
+})
 
 
 //Capturando o elemento do formulário
@@ -10,25 +14,64 @@ form.addEventListener("submit", (evento) => {
 
     const tarefa = evento.target.elements['tarefa']
 
-    criaElemento(tarefa.value)
+    const existe = itens.find( elemento => elemento.tarefa === tarefa.value )
+
+    const itemAtual = {
+        "tarefa": tarefa.value
+    }
+
+    if (existe) {
+        itemAtual.id = existe.id
+        
+        itemJaExistente(itemAtual)
+
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
+    } else {
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;
+
+    criaElemento(itemAtual)
+
+    itens.push(itemAtual)
+    }
+    localStorage.setItem("itens", JSON.stringify(itens))
 
     tarefa.value = ""
 })
 
 //Adicionando o elemento a minha lista
 
-function criaElemento(tarefa){
+function criaElemento(item){
     const novoItem = document.createElement('li')
 
     novoItem.classList.add("item")
-    novoItem.innerHTML = tarefa 
+    novoItem.innerHTML = item.tarefa 
+    novoItem.dataset.id = item.id
+
     lista.appendChild(novoItem)
 
-    const itemAtual = {
-        "tarefa": tarefa
-    }
+    novoItem.appendChild(botaoDeleta(item.id))
 
-    itens.push(itemAtual)
+}
+
+function itemJaExistente(item) {
+    alert("Este item já existe")
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = "X"
+
+    elementoBotao.addEventListener("click", function () {
+        deletaElemento(this.parentNode, id)
+    })
+
+    return elementoBotao
+}
+
+function deletaElemento(tag, id) {
+    tag.remove()
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
 
     localStorage.setItem("itens", JSON.stringify(itens))
 }
